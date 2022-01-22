79dab859ff78f7958e06518d0b7dcf3b4fbc25db
dwisusanto29 <dw.susanto@gmail.com>
2022-01-22 22:12:11 +0700

    Finisihing data,

--- converlog.py
+++ converlog.py
@@ -1,19 +1,36 @@
 import sys 
 import json
-import argparse
+import optparse
 
-fileinput = sys.argv[1]
-fileoutpu = sys.argv[2]
+def json_convertion(fileinput, fileoutput):
+    i = 1
+    result = {}
+    with open(fileinput) as f:
+        lines = f.readlines()
+        for line in lines:
+            result[i] = line
+            i += 1
+    output_file = open(fileoutput, "w")
+    json.dump(result, output_file, indent = 4, sort_keys = False)
+    output_file.close()
 
-i = 1
-result = {}
-with open(fileinput) as f:
-    lines = f.readlines()
-    for line in lines:
-        r = line.split('] ')
-        result[i] = {'timestamp': r[0].replace('[', ''), 'module': r[1].replace('[', ''), 'pid': r[2].replace('[', ''), 'message': r[3].replace('[', '')}
-        i += 1
+def text_convertion(fileinput, fileoutput):
+    output_file = open(fileoutput, "w")
+    with open(fileinput) as f:
+        lines = f.readlines()
+        for line in lines:
+            output_file.write(line)
+    
+if __name__=="__main__":
+    parser = optparse.OptionParser("usage: %prog fileinput [options] fileoutput")
+    parser.add_option("-t", "--type", dest="typefile", default="json", type="string" help="")
+    parser.add_option("-o", "--output", dest="output")
+    (options, args) = parser.parse_args()
 
-out_file = open(fileoutput, "w")
-json.dump(result, out_file, indent = 4, sort_keys = False)
-out_file.close()
+    output_file = options.output
+    type_file = options.typefile
+
+    if(type_file == "text"):
+        text_convertion(sys.argv[1], output_file)
+    else:
+        json_convertion(sys.argv[1], output_file )
